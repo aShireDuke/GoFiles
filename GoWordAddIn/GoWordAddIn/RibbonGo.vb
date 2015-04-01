e1c3aed8c75f60a896dc89c96e8f5ac199fae4c1
aShireDuke <addukesh@gmail.com>
2015-04-01 16:39:56 -0600

    Started over with AddIn to use VB instead of C#

---
+++ GoWordAddIn/GoWordAddIn/RibbonGo.vb
@@ -0,0 +1,22 @@
+﻿Imports Microsoft.Office.Tools.Ribbon
+
+Public Class RibbonGo
+
+    Private Sub RibbonGo_Load(ByVal sender As System.Object, ByVal e As RibbonUIEventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As RibbonControlEventArgs) Handles Button1.Click
+        System.Windows.Forms.MessageBox.Show("Hello you!")
+
+        Dim valReturn As Integer
+
+        'make the file dialog visible to the user 
+        With Globals.ThisAddIn.Application.FileDialog(Microsoft.Office.Core.MsoFileDialogType.msoFileDialogSaveAs)
+            .InitialFileName = "blaaarg"
+            valReturn = .Show()
+        End With
+        System.Windows.Forms.MessageBox.Show(valReturn)
+
+    End Sub
+End Class
